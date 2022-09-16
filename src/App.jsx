1c02ff6f11d47281ad763856bb9e99f33e6bd8fd
José Humanes Humanes <proyectos@josehumaneshumanes.com>
2022-09-16 08:43:02 +0200

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { useGlobalContext } from './context'
+import Search from './components/Search'
+import Meals from './components/Meals'
+import Modal from './components/Modal'
+import Favorites from './components/Favorites'
+
+function App() {
+
+  const { showModal, favorites } = useGlobalContext()
+
+  return (
+    <main>
+      <Search />
+      {favorites.length > 0 && <Favorites />}
+      <Meals />
+      {showModal && <Modal />}
+    </main>
+  )
+}
+
+export default App
